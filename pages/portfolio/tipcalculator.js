c2a013d720ed645c0c22d876db4df3a513f482be
Faith <70795111+fpatri01@users.noreply.github.com>
2026-06-25 06:06:08 -0400

    Adjust total calculation to include tax
    Updated total calculation to include tax.

--- pages/portfolio/tipcalculator.js
+++ pages/portfolio/tipcalculator.js
@@ -8,13 +8,12 @@ button.addEventListener('click', () => {
 let bill = Number(document.getElementById('bill').value);
 let tip = Number(document.getElementById('tip').value);
 //calculate the total per person
-let total = (tip/100)*bill + bill;
+let total = (tip/100)*bill + bill*1.07;
 let people = Number(document.getElementById('people').value);
 var total_each = total/people;
 var total_each = total_each.toFixed(2);
 
 //print total per person
-let result = document.getElementById("result");
 result.innerHTML = "$" + total_each;
 result.classList.add("visible");
 alert("The cost per person is $ " + total_each)
